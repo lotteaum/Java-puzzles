c39fef590fa898cec48c124d26169cd6f93fe600
lotteaum <matthieulotteau24@orange.fr>
2019-06-16 14:48:11 +0200

    Create Temperatures.java

---
+++ easy/Temperatures.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+/* Lien du puzzle : https://www.codingame.com/training/easy/temperatures */
+
+class Temperatures {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt(); // the number of temperatures to analyse
+        int result;
+        if (n==0) {
+            result = 0;
+        } else {
+            result = in.nextInt();
+        }
+        for (int i = 1; i < n; i++) {
+            int t = in.nextInt(); // a temperature expressed as an integer ranging from -273 to 5526
+            if (Math.abs(t)<Math.abs(result) || t>0 && t== -result){
+                result = t;
+            }
+        }
+
+        // Write an action using System.out.println()
+        // To debug: System.err.println("Debug messages...");
+
+        System.out.println(Integer.toString(result));
+    }
+}
